bba53442e601a18076b9420880112d2bfefda3f7
Joseph Nelson <joseph.nelson2012@gmail.com>
2016-11-01 21:45:04 -0400

    added a cubed normality distplot

--- normality_exp.py
+++ normality_exp.py
@@ -8,6 +8,7 @@ Created on Mon Sep 12 23:08:15 2016
 import numpy as np
 rand1 = np.random.normal(0,1,10000)
 rand2 = np.random.normal(0,1,10000)
+rand3 = np.random.normal(0,1,10000)
 
 # multipythem (or rand_squared)
 rand_sq = rand1 * rand2
@@ -19,3 +20,7 @@ sns.distplot(rand_sq)
 # conduct normality test, reject if p<0.05
 import scipy as sp
 sp.stats.mstats.normaltest(rand_sq)
+
+# TEST: cubing
+rand_cubed = rand1 * rand2 * rand3
+sns.distplot(rand_cubed)
