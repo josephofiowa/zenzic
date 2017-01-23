362876e43adf1d4d5d491c2c18f4a1e6c1da81ac
Joseph Nelson <joseph.nelson2012@gmail.com>
2017-01-23 00:52:30 -0500

    formatted to loop through links and add data to a df

--- obama-onion/obama-onion-scraper.py
+++ obama-onion/obama-onion-scraper.py
@@ -8,7 +8,6 @@ Created on Wed Jan 18 15:45:04 2017
 # standard imports
 from bs4 import BeautifulSoup
 import requests
-from time import sleep
 
 # grab the full text of the feature page
 r = requests.get('http://www.theonion.com/interactive/obama/2008/28')
@@ -28,39 +27,68 @@ onion = pd.DataFrame(data=links, columns=['links'])
 onion.head()
 onion.to_csv('onion_links.csv')
 
+'''
+Using the links to collect data
+'''
+
 # load csv
 onion = pd.read_csv('onion_links.csv')
 onion.head()
 
-link_to_visit = 'http://theonion.com'+ onion.links[1]
-r = requests.get(link_to_visit)
-soup = BeautifulSoup(r.text)
-
-# article body
-for para in soup.find_all('p'):
-    if 'Copyright 2017 Onion' in para.text:
-        pass
-    elif 'Give your spam filter something to do.' in para.text:
-        pass
-    else:
-        print para.text
-
-# article title
-soup.find('header', attrs={'class':'electoral-retro-promo-header'}).find('a').text.strip()
-
-# date
-soup.find('span', attrs={'class':'content-published'}).text.strip()
-
-# content tags
-for tag in soup.find('span', attrs={'class':'content-tags'}).find_all('a'):
-    print tag.text.strip()
-
-# drop 
-for x in enumerate(onion.links):
-    
-    
-    
+# drop all graphics from dataset
+for i, x in enumerate(onion.links):
     if '/graphic/' in x:
-        print True
+        onion.drop(i, inplace=True)
 
+# confirm drops
+onion.shape
 
+# build dataset
+stories = pd.DataFrame(columns=['link','title','date','tag0','tag1','tag2','tag3','tag4','body'])
+
+# grab these to be nice to The Onion
+from time import sleep
+import random 
+
+for j, slug in enumerate(onion.links):    
+    # request main feature page each time to be legit
+    requests.get('http://www.theonion.com/interactive/obama')
+    # wait to grab a link
+    sleep(random.uniform(.1,6))    
+    
+    # begin the real process
+    link_to_visit = 'http://theonion.com'+ slug
+    r = requests.get(link_to_visit)
+    soup = BeautifulSoup(r.text)
+    
+    # grab article title      
+    title = soup.find('header', attrs={'class':'electoral-retro-promo-header'}).find('a').text.strip()
+    # grab article date
+    date = soup.find('span', attrs={'class':'content-published'}).text.strip()
+    # inelegantly reset tags each iteration
+    tags ={'tag0':None,
+           'tag1':None,
+           'tag2':None,
+           'tag3':None,
+           'tag4':None
+           }
+    # grab tags for article - but only as many tags (<=4) as appear for an article
+    for i, x in enumerate(soup.find('span', attrs={'class':'content-tags'}).find_all('a')):
+        tags['tag{0}'.format(i)]=x.text.strip()
+    
+    # body - inelegantly ignore Copyright and spam filtering p text tags
+    body = []
+    for para in soup.find_all('p'):
+        if 'Copyright 2017 Onion' in para.text:
+            pass
+        elif 'Give your spam filter something to do.' in para.text:
+            pass
+        else:
+            body.append(para.text)
+    
+    # add to df
+    stories.loc[len(stories)]=[link_to_visit, title, date, tags['tag0'], tags['tag1'], tags['tag2'], tags['tag3'], tags['tag4'], body]    
+    # pause a random amount of time - about a minute each
+    
+    print 'Completed ' + str(j)
+    sleep(random.uniform(.1,70))
