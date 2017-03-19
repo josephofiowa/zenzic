aab76c6b8b5b80c24e31ea1de792d3fa4f2670b0
Joseph Nelson <joseph.nelson2012@gmail.com>
2017-03-19 04:12:09 -0400

    obtained historical weather forecast data

--- federal-snow/forecast-data.py
+++ federal-snow/forecast-data.py
@@ -1,72 +1,80 @@
-import darksky as ds
+import pandas as pd
 import forecastio
-import datetime
+from datetime import *
+import numpy as np
 
-# Haven't saved your API key as an environment variable?
-ds.key('545461fdd9504a9342a3f4c7d40173b7')
-
-
-# Look up interesting storms
-ds.interesting()
-
-# Get a brief forecast for a latitude/longitude
-ds.brief(38.8977, -77.0365)
-38.8977,-77.0365
-
-# Or for a given address
-ds.brief('155 9th St San Francisco, CA')
-
-# Alternatively, get the full forecast
-ds.forecast(37.775002, -122.418297)
-
-# Same goes for an address
-ds.forecast('155 9th St San Francisco, CA')
-
-# Fetch the latitude/longitude manually
-ds.location('155 9th St San Francisco, CA')
-
-r = requests.get('https://api.darksky.net/forecast/545461fdd9504a9342a3f4c7d40173b7/38.8977,-77.0365?time=[2017]-[03]-[13]T[12]:[00]:[00]')
-data = json.loads(r.content)
-
-data['daily']['data']
-
-import forecastio
-forecast = forecastio.load_forecast('545461fdd9504a9342a3f4c7d40173b7', 38.8977, -77.0365)
-forecast.hourly().summary
-
-lat  = 38.8977
-lng  = -77.0365
-now = datetime.datetime(2017, 3, 13, 0, 0, 0)
-api_key = ''
-forecast = forecastio.load_forecast(api_key, lat, lng, time=now)
-forecast.daily()
-
-byHour = forecast.hourly()
-byHour.temperature
-
-for hourlyData in byHour.data:
-    print hourlyData.precipProbability
-    print hourlyData.temperature
-
-for hourlyData in byHour.data:
-    print hourlyData.precipProbability
+'''
+Three steps here
+1) Create list of dates for which we want historical weather forecast data
+2) Use Dark Sky API to grab data for those dates
+3) Export data
+'''
 
 
-byHour.data[23].precipProbability
-
-forecast.currently().temperature
-
-# StackOverflow http://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python
-from datetime import timedelta, date
-
+'''
+1) Create list of dates for which we want historical weather forecast data
+'''
+# function to produce all dates between a start and end
+# thanks, StackOverflow http://stackoverflow.com/questions/1060279/iterating-through-a-range-of-dates-in-python
 def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-dates = []
+# set defaults (we only have good Google weekly data for these dates)
 start_date = date(2012, 3, 25)
 end_date = date(2017, 3, 19)
+
+# create date list
+dates = []
 for single_date in daterange(start_date, end_date):
     dates.append(datetime.datetime.combine(single_date, datetime.time.min))
 
-len(dates)
+'''
+2) Use Dark Sky API to grab data for those dates
+https://darksky.net/dev/docs
+https://github.com/ZeevG/python-forecast.io
+'''
+
+# set defaults - this is for the White House
+lat  = 38.8977
+lng  = -77.0365
+
+# if you didn't set an env variable...
+api_key = ''
+
+# make df
+weather = pd.DataFrame(columns=['date','summary', 'temp','precip', 'precip_intensity', 'precip_type', 'wind', 'visibility'])
+
+# get data
+for date in dates:
+    forecast = forecastio.load_forecast(api_key, lat, lng, time=date)
+    # make hourly
+    byHour = forecast.hourly()
+    # get 23rd hour data (last for that calendar date - 11PM) 
+    try:  
+        summary = byHour.data[23].summary
+        temp = byHour.data[23].temperature
+        precip = byHour.data[23].precipProbability
+        precipIntensity = byHour.data[23].precipIntensity
+        if precipIntensity!=0:
+            precipType = byHour.data[23].precipType
+        else:
+            precipType = np.nan
+        wind = byHour.data[23].windSpeed
+        visibility = byHour.data[23].visibility
+    except:
+        pass
+    # add to df    
+    weather.loc[len(weather)]=[date, summary, temp, precip, precipIntensity, precipType, wind, visibility]
+
+'''
+3) Export data
+'''
+
+# check it out
+weather.shape
+weather.head()
+weather.tail()
+
+# export it
+weather.to_csv('forecasts.csv', encoding='utf-8')
