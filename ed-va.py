92b0ff47a30808674d8d11ab48a5c504de915e6c
Joseph Nelson <joseph.nelson2012@gmail.com>
2017-05-31 00:27:05 -0400

    added counter

--- ed-va.py
+++ ed-va.py
@@ -5,5 +5,5 @@ count = 0
 while True:
 	requests.get('https://edforvirginia.com/meet-ed/?utm_source=justinlhiggins&utm_campaign=direct&utm_medium=banner&utm_content=300x250-A4')
 	sleep(3)
-	count = count=+1
+	count = count+1
 	print('Ping. . . ' + str(count))
